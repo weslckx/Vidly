42ba782d9c793bffdd67f7e0e257a78739a4a006
Wesley Dierckx <dierckxwesley@outlook.com>
2020-11-11 12:36:49 +0100

    Adding custom validation to Customer (Business rule: 18+ for membership)

--- Vidly/Vidly/Models/Customer.cs
+++ Vidly/Vidly/Models/Customer.cs
@@ -9,11 +9,12 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage ="Please enter customer's name")]
         [StringLength(255)]
         public string Name { get; set; }
         [DisplayFormat(ApplyFormatInEditMode =true, DataFormatString ="{0:dd/MM/yyyy}")]
         [Display(Name="Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         [Display(Name ="Membership Type")]
---
+++ Vidly/Vidly/Models/Min18YearsIfAMember.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Min18YearsIfAMember:ValidationAttribute
+    {
+        // override ValidationResult (option with 2 parameters)
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance; //get acces to object, cast it to customer
+
+            if (customer.MembershipTypeId==0 ||customer.MembershipTypeId == 1) //"magic number", need to refactor it
+                return ValidationResult.Success;
+
+            if (customer.Birthdate == null)
+                return new ValidationResult("Birthday is required");
+
+            var age = DateTime.Now.Year - customer.Birthdate.Value.Year; // not completly right, but out of scope for this exc
+
+            return (age >= 18)
+                ? ValidationResult.Success 
+                : new ValidationResult("Customer needs to be at least 18 years to go on a membership");
+         
+        }
+    }
+}
