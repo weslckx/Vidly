d01edcafb07b7816f3a820e20097b43d58fc5713
Wesley Dierckx <dierckxwesley@outlook.com>
2020-11-14 17:13:13 +0100

    Initialised AutoMapper for Movies

---
+++ Vidly/Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public DateTime AddDate { get; set; }
+        [Required]
+        [Range(1, 20)]
+        public int Stock { get; set; }
+
+
+        public byte GenreId { get; set; } 
+
+    }
+}
